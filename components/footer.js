a3aa9c6c0f2d28614c63f3724cb08eb6235651b3
s22016@std.it-college.ac.jp <s22016@std.it-college.ac.jp>
2023-02-21 01:42:59 +0000

    seccond commit

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,19 @@
+import Container from 'components/container'
+import Logo from 'components/logo'
+import Social from 'components/social'
+import styles from 'styles/footer.module.css'
+
 export default function Footer() {
-	return <footer>FOOTER</footer>
+	return (
+		<footer className={styles.wrapper}>
+		  <Container>
+		    <div className={styles.flexContainer}>
+		      <Logo />
+		      <Social />
+		    </div>
+		  </Container>
+		</footer>
+	)
 }
 
 
